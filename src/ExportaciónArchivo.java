221858b3c33375a6b2619141b523a405e65705ab
dani <daniel.hermar.13@educa.jcyl.es>
2026-03-10 01:13:17 +0100

    Inclusión varios archivos

---
+++ src/ExportaciónArchivo.java
@@ -0,0 +1,15 @@
+
+import java.nio.file.Path;
+import java.util.List;
+
+public class ExportaciónArchivo {
+	private Path refArchivo;
+	
+	public void exportaciónArchivo(String rutaArchivo) {
+		
+	}
+	
+	public void guardar(List <String> contenidos) {
+		
+	}
+}
---
+++ src/VistaGeneral.java
@@ -0,0 +1,54 @@
+
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * Posee todas las funciones de impresión
+ */
+public class VistaGeneral {
+	private final static String FORMATO_PRINTF_MOSTRARTEXTO;
+	private final static String FORMATO_PRINTF_MOSTRARAVISO;
+	private static Scanner scEntrada;
+	
+	public static Scanner getScEntrada() {	// método getInstance()
+		if (scEntrada == null) {
+			scEntrada = new Scanner(System.in); 
+		}
+		
+		return scEntrada;
+	}
+	
+	public static void mostrarTexto(String texto) {
+		System.out.printf();
+	}
+	
+	public static void mostrarAviso(String texto) {
+		System.out.printf();
+	}
+
+	public static void mostrarTítulo(String texto) {
+		System.out.printf();
+	}
+
+	public static void mostrarTítulo2(String texto) {
+		System.out.printf();
+	}
+	
+	public static int pedirNúmero(String texto) {
+		System.out.printf("Elija opción: ");
+	}
+
+	public static void pausa(String texto) {
+		System.out.printf();
+	}
+
+	public static boolean pedirConfirmación(String texto) {
+		System.out.printf("¿Quieres salir del programa?");	// s - sí, n - no
+	}
+	
+	public void mostrarLista(List<String>) {
+		for (String elemento : ) {
+			
+		}
+	}
+}
