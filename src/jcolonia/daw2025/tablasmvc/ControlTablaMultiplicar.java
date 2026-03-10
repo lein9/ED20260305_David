c142acbfc546e34b6f2c7c31dcd7defc05dd5316
dani <daniel.hermar.13@educa.jcyl.es>
2026-03-10 12:27:33 +0100

    10/marzo/2026 - Cambios en 'ControlTablaMultiplicar' y en 'VistaGeneral'

--- src/jcolonia/daw2025/tablasmvc/ControlTablaMultiplicar.java
+++ src/jcolonia/daw2025/tablasmvc/ControlTablaMultiplicar.java
@@ -1,103 +1,122 @@
 package jcolonia.daw2025.tablasmvc;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 /**
-* Núcleo de aplicación de consola de texto con menús. Aplicación
-* de texto usando tablas de multiplicar infantiles clásicas. 
-*
-* @author dani + Alejandro Gómez Juez
-* @version 1.0_dani (+ 1.0_AG1)
-*/
+ * 
+ */
 public class ControlTablaMultiplicar {
-	/** Formato tipo «printf» para el nombre del archivo de
-	* exportación.
-	*/
-	public static final String FORMATO_RUTA_ARCHIVO_EXPORTACIÓN=
-		"tabla del %02d.txt";
+	/** Formato tipo «printf» para el nombre del archivo de exportación. */
+	public static final String FORMATO_RUTA_ARCHIVO_EXPORTACIÓN = "tabla del %02d.txt";
 	
 	/** Tabla de multiplicar activa. */
 	private TablaMultiplicar tabla;
-
-	public ControlTablaMultiplicar(){
+	
+	/**
+	 * 
+	 */
+	public ControlTablaMultiplicar() {
 		init();
 	}
-
-
+	
+	/* en realidad usarémos VistaMenú, que hereda de VistaGeneral (por ahora no la ponemos) */
+	//private VistaGeneral vista;
+	
 	/**
-	* Pide al usuario un número y prepara la primera
-	* tabla activa.
-	*/
-	public void init(){
+	 * Pide al usuario un número y prepara la primera tabla activa. 
+	 */
+	public void init() {
 		cambiarTabla();
 	}
-
+	
 	/**
-	* Gestión del menú principal. Desde este menú
-	* se ejecutan las opciones disponibles a elección del usuario.
-	* A la salida del menú se finaliza el programa.
-	*/
-	public void buclePrincipal(){
+	 * Gestión del menú principal. Desde este menú se ejecutan las opciones disponibles a elección del
+	 * usuario. 
+	 * A la sadila del menú se finaliza el programa. 
+	 */
+	public void buclePrincipal() {
 		VistaMenú menú;
 		int opción;
 		
-		menú=new VistaMenú("Tablas de multiplicar",OPCIONES_MENÚ_PRINCIPAL);
+		// título, array de opciones
+		menú = new VistaMenú("Tablas de multiplicar", OPCIONES_MENÚ_PRINCIPAL);
 		
-		do{
+		do {
 			menú.mostrarOpciones();
-			opción=menú.pedirOpción();
+			opción = menú.pedirNúmero(); // tal vez .pedirOpción() del menú original
 			
-			switch(opción){
-			case 1: // Mostrar tabla
+			switch (opción) {
+			case 1:	// Mostrar tabla
 				mostrarTabla();
 				break;
-			case 2: //Cambiar tabla
-				cambiarTabla();
-				break;
-			case 3: // Exportar tabla
+			case 2: // Exportar tabla
 				exportarTabla();
 				break;
-			case 0: // Salir
+			case 3: // Cambiar tabla
+				cambiarTabla();
 				break;
-			default: // Opciones no implementadas
+			case 0:	// Salir
+
+				break;
+			default:	// Otras opciones no implementadas -opciónNoDisponible()-
 				opciónNoDisponible();
 				break;
 			}
-			
-		} while (opción!=0);
+		} while (opción != 0);
 		
+		// mostrado cuando el usuario introduce 0
 		VistaGeneral.mostrarAviso("FIN");
-		
 	}
 	
 	/**
-	* Muestra por pantalla -envía a la salida estándar-
-	* los productos correspondientes a la tabla activa.
-	*/
-	private void mostrarTabla(){}
+	 * Muestra por pantalla -envía a la salida estándar- los productos correspondientes a la tabla activa. 
+	 */
+	private void mostrarTabla() {
+		tabla.toString();
+	}
 	
 	/**
-	* Cambia la tabla activa por otra elegida por el usuario.
-	*/
-	private void cambiarTabla(){
+	 * Envía a un archivo los productos correspondientes a la tabla activa. 
+	 */
+	private void exportarTabla() {
+		Path refArchivo = Path.of( String.format(FORMATO_RUTA_ARCHIVO_EXPORTACIÓN) );
+		try {
+			Files.deleteIfExists(refArchivo);
+			Path archivo = Files.createFile(refArchivo);
+			
+			BufferedWriter escribe;
+			escribe = Files.newBufferedWriter(archivo, Charset.forName("windows-1252"));	// tiene el charset
+			PrintWriter salida = new PrintWriter(escribe);
+
+			tabla.toString();
+
+			salida.close();
+		} catch (IOException e) {
+			System.err.printf("Error de escritura: %s", e.getLocalizedMessage());
+		}
+	}
+	
+	/**
+	 * Cambia la tabla activa por otra elegida por el usuario. 
+	 */
+	private void cambiarTabla() {
 		int n;
 		
-		VistaGeneral.pedirNúmero("Introduzca el número para la tabla");
-		
-		tabla=new TablaMultiplicar(n);
+		n = VistaGeneral.pedirNúmero("Introduzca el número de la tabla a la que cambiar: ");
+		// falta lo del Scanner
+		tabla = new TablaMultiplicar(n);
 		tabla.generarTabla();
 	}
-
-	/**
-	* Envía a un archivo
-	* los productos correspondientes a la tabla activa.
-	*/
-	private void exportarTabla(){}
 	
 	/**
-	 * Muestra un mensaje de aviso indicando que 
-	 * la opción elegida no está disponible.
-	*/
-	private void opciónNoDisponible(){}
-
-
-
-
+	 * Muestra un mensaje de aviso indicando que la opción elegida no está disponible. 
+	 */
+	private void opciónNoDisponible() {
+		
+	}
 }
--- src/jcolonia/daw2025/tablasmvc/VistaGeneral.java
+++ src/jcolonia/daw2025/tablasmvc/VistaGeneral.java
@@ -2,6 +2,8 @@ package jcolonia.daw2025.tablasmvc;
 import java.util.List;
 import java.util.Scanner;
 
+import jcolonia.daw2025.ut5.menu.excepciones.NoEstáEntreOpcionesException;
+
 /**
  * Posee todas las funciones de impresión
  */
@@ -9,6 +11,11 @@ public class VistaGeneral {
 	private final static String FORMATO_PRINTF_MOSTRARTEXTO;
 	private final static String FORMATO_PRINTF_MOSTRARAVISO;
 	private static Scanner scEntrada;
+	
+//	{
+//		FORMATO_PRINTF_MOSTRARTEXTO = "";
+//		FORMATO_PRINTF_MOSTRARAVISO = "";
+//	}
 	
 	public static Scanner getScEntrada() {	// método getInstance()
 		if (scEntrada == null) {
@@ -23,27 +30,139 @@ public class VistaGeneral {
 	}
 	
 	public static void mostrarAviso(String texto) {
-		System.out.printf();
+		System.out.printf("Estamos en la tabla del %s", scEntrada.nextLine());
 	}
 
+	/**
+	 * Imprime el título del menú con un subrayado para separarlo de las opciones.
+	 * Sobre el formato: antes del título hay un espacio y el subrayado será con '=' y sobresaldrá un 
+	 * caracter por ambos lados:
+	 * <div><pre>
+	 * Título 
+	 *========
+	 * </pre>
+	 * </div>
+	 * 
+	 * @param texto título a mostrar
+	 */
 	public static void mostrarTítulo(String texto) {
-		System.out.printf();
+		StringBuffer base;
+		base = new StringBuffer();
+		base.append("==");	// que sobresalga un carácter por ambos lados
+		
+		// Ponemos tantas veces el carácter báse, '=', como longitud tiene el título 
+		for (int i=0; i < texto.length(); i++) {
+			base.append("=");
+		}
+		
+		System.out.printf(" %s %n", texto);
+		System.out.printf("%s%n", base);
 	}
 
+	/**
+	 * Imprime un título encima de las opciones del menú. 
+	 * Sobre el formato: antes del título hay un espacio y el subrayado será con '=' y sobresaldrá un 
+	 * caracter por ambos lados:
+	 * <div><pre>
+	 * Título 
+	 *========
+	 * </pre>
+	 * </div>
+	 * 
+	 * @param texto título que se pone encima de las opciones. 
+	 */
 	public static void mostrarTítulo2(String texto) {
-		System.out.printf();
+		/* tal vez sea para mostrar un título nuevo en caso de cambio de este */
+		StringBuffer base;
+		base = new StringBuffer();
+		base.append("==");	// que sobresalga un carácter por ambos lados
+		
+		// Ponemos tantas veces el carácter báse, '=', como longitud tiene el título 
+		for (int i=0; i < texto.length(); i++) {
+			base.append("=");
+		}
+		
+		System.out.printf(" %s %n", texto);
+		System.out.printf("%s%n", base);
 	}
 	
+	/**
+	 * Muestra las opciones del menú, que se pasa como parámetro, y saca un texto pidiendo un número, 
+	 * que corresponderá a una de las opciones del menú. 
+	 * El formato será:
+	 * <div><pre>
+	 *  1) &lt;opción&gt;
+	 *  2) &lt;opción&gt;
+	 *  3) &lt;opción&gt;
+	 *  .  .  .
+	 *  0) Salir
+	 *  
+	 * Introduzca número de opción (0 para salir):
+	 * </pre>
+	 * </div>
+	 * 
+	 * @param texto cadena de texto con las opciones para el menú separadas por ','. 
+	 * @return entero que indica la opción elegida. 
+	 */
 	public static int pedirNúmero(String texto) {
-		System.out.printf("Elija opción: ");
+		String[] opciones;	// para almacenar las opciones del menú
+		int elección;
+		boolean salir;	/* para salir del bucle do-while */
+		
+		opciones = new String[texto.split(",").length];
+		// considero que el texto es un CSV
+		for (int i=0; i<texto.split(",").length; i++) {
+			opciones[i] = texto.split(",")[i];
+		}
+		
+		/* hay que darle un valor, porque en el do-while puede considerar que no se le asigna un valor */
+		elección = -1;
+		salir = false;
+		
+		// IMPRIMO LAS OPCIONES
+		for (int i=0; i<opciones.length; i++) {
+			System.out.printf("  %d) %s%n", i+1, opciones[i]);
+		}
+		System.out.printf("  %d) %s%n", 0, "Salir");
+		
+		System.out.println();
+		
+
+		// SOLICITO NÚMERO Y OPERACIONES PARA CAPTURARLO
+		System.out.printf("Introduzca número de opción (0 para salir): ");
+		
+		do {
+			try {
+				elección = Integer.parseInt( scEntrada.nextLine() );
+				if (elección == 0) {
+					System.exit(0);	// Salida sin anomalía
+				} else if (elección < 1 || elección > opciones.length) {
+					// se usaría NumberFormatException, pero cree una
+					throw new NumberFormatException("Entrada inválida");
+				}
+				salir = true;
+			} catch (NumberFormatException e) {
+				System.out.printf("Por favor introduce un número válido%n");
+			}
+		} while (!salir);
+		
+		scEntrada.close();
+		return elección;
 	}
 
+	/**
+	 * Imprime un mensaje 'de pausa', hasta que se presione una tecla.  
+	 * 
+	 * @param texto texto que se mostrará en el mensaje
+	 */
 	public static void pausa(String texto) {
-		System.out.printf();
+		System.out.println(texto);	// será tipo: "Pulsa una tecla para continuar..."
+		scEntrada.nextLine();	// cuando se presione se pasará a la función que cierra el escaner
+		scEntrada.close();
 	}
 
 	public static boolean pedirConfirmación(String texto) {
-		System.out.printf("¿Quieres salir del programa?");	// s - sí, n - no
+		System.out.printf("¿Quieres salir del programa?  →  s - sí | n - no");
 	}
 	
 	public void mostrarLista(List<String>) {
